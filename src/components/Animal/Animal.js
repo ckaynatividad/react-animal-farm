235a0cf154e290f0f4ff97ac2dcb01a8ea6570b9
ckaynatividad <natividad.ckay@gmail.com>
2021-11-30 15:11:47 -0800

    demo

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,5 +1,12 @@
 import React from 'react'
+import './Animal.css'
 
-export default function Animal() {
-  return <></>
+export default function Animal({ name, left, top, type, says }) {
+  return (
+    <div className="animal" style={{ name, left, top, type, says }}>
+      <img alt={name} src={`${process.env.PUBLIC_URL}/animals/${type}.svg`} />
+      <span className="name">{name}</span>
+      <span>{says}</span>
+    </div>
+  )
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,19 @@
 import React from 'react'
 import './Main.css'
 
+import { animals } from '../../data'
+
+import Animal from '../Animal/Animal'
+
+import background from '../../background.png'
+
 export default function Main() {
-  return <main></main>
+  const styles = { backgroundImage: `url(${background})` }
+  return (
+    <main style={styles}>
+      {animals.map((animal) => (
+        <Animal key={animal.key} {...animal} />
+      ))}
+    </main>
+  )
 }

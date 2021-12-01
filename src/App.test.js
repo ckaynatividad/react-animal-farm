85c48b6997beaf18e10d39ecc2aca120ca17e35d
ckaynatividad <natividad.ckay@gmail.com>
2021-12-01 11:34:18 -0800

    4

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App'
 
 test('renders header', () => {
   render(<App />)
-  const linkElement = screen.getByText(/ animal farm /i)
+  const linkElement = screen.getByText('animal farm')
   expect(linkElement).toBeInTheDocument()
 })

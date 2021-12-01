af3252ef0a120797fcfc7191f1f76c8dc4afae8b
ckaynatividad <natividad.ckay@gmail.com>
2021-12-01 11:29:30 -0800

    2

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App'
 
 test('renders header', () => {
   render(<App />)
-  const linkElement = screen.getByText(/zodiac/i)
+  const linkElement = screen.getByText(/ /)
   expect(linkElement).toBeInTheDocument()
 })
